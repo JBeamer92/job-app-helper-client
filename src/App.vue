e546d2747e773f6d7ffb598d5d7f252a5b527cf9
JBeamer92 <jared.c.beamer@gmail.com>
2020-11-23 22:12:24 -0500

    Added landing page, vuetify plugin

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,50 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+<!--Navigation-->
+    <v-app-bar
+      app
+      color="#868685"
+      dark
+      hide-on-scroll
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <div class="d-flex align-center">
+        <v-toolbar-title>Job App Helper</v-toolbar-title>
+      </div>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>Register</v-btn>
+        <v-btn text>Login</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+<!--Body-->
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+<!--Footer-->
+    <v-footer
+        app
+        padless
+        color="#868685"
+        dark
+    >
+      <v-col class="text-center" cols="12">
+        Copyright&nbsp; &copy; {{ new Date().getFullYear() }}, <strong>JBDevApps</strong>
+      </v-col>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
